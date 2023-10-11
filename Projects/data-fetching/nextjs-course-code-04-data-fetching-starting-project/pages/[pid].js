2d793a88e2150467d0280bc5112d63d3860174d4
syzgod <viktor.berczeli@gmail.com>
2023-10-11 12:06:10 +0200

    Add dynamic pages

---
+++ Projects/data-fetching/nextjs-course-code-04-data-fetching-starting-project/pages/[pid].js
@@ -0,0 +1,43 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+const ProductDetailPage = (props) => {
+  const { loadedProduct } = props;
+
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { pid: 'p1' } },
+      { params: { pid: 'p2' } },
+      { params: { pid: 'p3' } },
+    ],
+    fallback: false,
+  };
+};
+
+export default ProductDetailPage;
